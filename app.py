4cb515688f64779132047a98c06097f6b94caecb
Koushal kumar <kushwahakoushal78@gmail.com>
2025-09-05 09:45:35 +0530

    cors problem

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app = FastAPI(title="Resume Analyzer (Gemini + Files)")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],   # or ["http://localhost:3000"]
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
